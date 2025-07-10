da7eef6c76437e62b679d76a5c8a8b1d26cba775
Ankush Rai <akrai@Ankushs-MacBook-Pro-3.local>
2025-07-10 13:58:22 -0700

    SRT algo

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 void run_fcfs(Process *procs, int n);
 void run_sjf(Process *procs, int n);
+void run_srt(Process *procs, int n);
 
 int main() {
     Process procs[MAX_PROCESSES];
@@ -15,6 +16,11 @@ int main() {
     // Run SJF
     generate_processes(procs, &n, 1);
     run_sjf(procs, n);
+
+    // Run SRT
+    generate_processes(procs, &n, 2);
+    run_srt(procs, n);
+
 
     return 0;
 }
---
+++ src/srt.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "process.h"
+
+void run_srt(Process *procs, int n) {
+    int time = 0, completed = 0;
+    int total_turnaround = 0, total_waiting = 0, total_response = 0;
+    int current = -1;
+    int first_response[MAX_PROCESSES] = {0};
+
+    printf("\n--- SRT (Shortest Remaining Time First) Scheduling ---\n");
+
+    while (completed < n && time <= 100) {
+        int shortest = -1;
+        for (int i = 0; i < n; i++) {
+            if (procs[i].arrival_time <= time && procs[i].remaining_time > 0) {
+                if (shortest == -1 || procs[i].remaining_time < procs[shortest].remaining_time) {
+                    shortest = i;
+                }
+            }
+        }
+
+        if (shortest == -1) {
+            time++;
+            continue;
+        }
+
+        if (!procs[shortest].started) {
+            procs[shortest].start_time = time;
+            procs[shortest].started = 1;
+            first_response[shortest] = time - procs[shortest].arrival_time;
+        }
+
+        procs[shortest].remaining_time--;
+        time++;
+
+        if (procs[shortest].remaining_time == 0) {
+            procs[shortest].finish_time = time;
+
+            int turnaround = procs[shortest].finish_time - procs[shortest].arrival_time;
+            int waiting = turnaround - procs[shortest].burst_time;
+            int response = first_response[shortest];
+
+            total_turnaround += turnaround;
+            total_waiting += waiting;
+            total_response += response;
+
+            completed++;
+        }
+    }
+
+    printf("Avg Turnaround Time: %.2f\n", (float)total_turnaround / n);
+    printf("Avg Waiting Time   : %.2f\n", (float)total_waiting / n);
+    printf("Avg Response Time  : %.2f\n", (float)total_response / n);
+    printf("Throughput         : %.2f\n", (float)n / time);
+}
